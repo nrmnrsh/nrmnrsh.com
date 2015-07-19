04394cdd537e655d2cff41a5592513b8115602d9
Norman Rusch <nrmnrsh@Normans-Air.fritz.box>
2015-07-19 16:26:02 +0200

    Update release task to push to gh-pages

--- resources/grunt/shell.js
+++ resources/grunt/shell.js
@@ -4,14 +4,24 @@ module.exports = function(grunt) {
 			command: [
 				'rm -rf release',
 				'mkdir release',
-				'git clone git@github.com:nrmnrsh/nrmnrsh.com.git release'
+				'git clone git@github.com:nrmnrsh/nrmnrsh.com.git release',
+				'cd release',
+				'git checkout gh-pages'
 			].join(' && ')
 		},
 		compass: {
-
+			command: [
+				'echo "compile compass"'
+			].join(' && ')
 		},
 		release: {
-
+			command: [
+				'cd release',
+				'git add .',
+				'git commit -m "Update gh-pages to latest version"',
+				'git push',
+				'open "https://nrmnrsh.github.io/nrmnrsh.com/"'
+			].join(' && ')
 		}
 	});
 
