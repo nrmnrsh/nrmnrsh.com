3a57431c881c2d9f5bbfd933598b3df3dd2ddf37
Norman Rusch <nrmnrsh@MacBook-Bro.fritz.box>
2016-06-18 14:22:26 +0200

    Add node-reset-scss

--- resources/grunt/sass.js
+++ resources/grunt/sass.js
@@ -1,6 +1,11 @@
 module.exports = function(grunt) {
 	grunt.config('sass', {
 		all: {
+			options: {
+				includePaths: [
+					require('node-reset-scss').includePath
+				]
+			},
 			files: {
 				'release/styles/styles.css': 'sources/styles/styles.scss'
 			}
