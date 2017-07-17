8e66530007ff350364615045379696504f7ba32e
Norman Rusch <norman.rusch@moccu.com>
2017-07-17 19:43:52 +0200

    Drop background image from intro module
    incl. noise function

--- sources/js/src/Application.js
+++ sources/js/src/Application.js
@@ -1,15 +1,13 @@
 import Application from 'picnic/core/app/Application';
 import Geppetto from 'backbone.geppetto';
 
-import Intro from 'app/modules/intro/commands/Initialize';
-
 
 class Context extends Geppetto.Context {
 
 	initialize() {
 		this.wireCommands({
 			'application:start': [
-				Intro
+
 			]
 		});
 	}
