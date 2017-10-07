e28d167cfe40a818e0a1f35cfa7f339666101835
Norman Rusch <webmaster@grind-it.de>
2017-10-07 21:19:57 +0200

    Remove options from slugify helper

--- sources/hbs/helpers/slugify.js
+++ sources/hbs/helpers/slugify.js
@@ -1,6 +1,6 @@
 var slugify = require('helper-slugify');
 
-module.exports.register = function (Handlebars, options) {
+module.exports.register = function (Handlebars) {
 	Handlebars.registerHelper('slugify', function(str) {
 		return slugify(str);
 	});
