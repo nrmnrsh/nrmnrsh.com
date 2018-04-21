a6e30618f65d84e96ff7d6ee6b74cdfb30d10261
Norman Rusch <me@nrmnrsh.com>
2018-04-21 15:42:31 +0200

    Cleanup critical

--- sources/js/critical.js
+++ sources/js/critical.js
@@ -1,11 +1,9 @@
 (function(src) {
-	const
-		doc = document,
-		html = doc.documentElement,
-		webp = new Image()
-	;
+	const doc = document;
+	const html = doc.documentElement;
 
 	// Test for webp support:
+	const webp = new Image();
 	webp.onerror = () => html.classList.add('supports-no-webp');
 	webp.onload = () => html.classList.add('supports-webp');
 	webp.src = 'data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vuUAAA=';
@@ -15,13 +13,13 @@
 		// (!!window.Proxy || import('proxy-polyfill')),
 		(!!window.IntersectionObserver || import('intersection-observer')),
 	]).then(() => {
-		var script = document.createElement('script');
+		var script = doc.createElement('script');
 		script.type = 'text/javascript';
 		script.charset = 'utf-8';
 		script.async = true;
 		script.defer = true;
 		script.src = src;
 		script.onload = () => html.classList.add('is-ready');
-		document.body.appendChild(script);
+		doc.body.appendChild(script);
 	});
 })('/js/app.pkg.js');
