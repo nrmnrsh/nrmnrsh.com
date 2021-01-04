b564349c284d000201e3bae9a5e647f167933698
Norman Rusch <webmaster@grind-it.de>
2021-01-04 09:31:50 +0100

    Disable logrocket

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,14 @@ __webpack_public_path__ = window.app.static_url; // eslint-disable-line no-undef
 
 import {Context} from 'pacto';
 import {Action as Analytics} from 'components/analytics/actions/InitializeLazy';
-import {Action as Logrocket} from 'components/logrocket/actions/InitializeLazy';
+// import {Action as Logrocket} from 'components/logrocket/actions/InitializeLazy';
 import {Action as Serviceworker} from 'components/serviceworker/actions/Initialize';
 
 
 const context = new Context();
 context.actions.add('app:run', [
 	Analytics,
-	Logrocket,
+	// Logrocket,
 	Serviceworker
 ]);
 context.trigger('app:run');
